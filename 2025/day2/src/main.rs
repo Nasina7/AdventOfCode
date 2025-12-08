71e5e64fcf6d68081b4e7092f3811fe74ba933ca
Nasina7 <38535607+Nasina7@users.noreply.github.com>
2025-12-07 19:52:57 -0500

    2025 Day 2
    Also added a README

---
+++ 2025/day2/src/main.rs
@@ -0,0 +1,82 @@
+use std::error::Error;
+
+struct Aoc2025Day2 {
+    ranges: Vec<(usize, usize)>,
+}
+
+impl Aoc2025Day2 {
+    fn new(filename: &str) -> Result<Aoc2025Day2, Box<dyn Error>> {
+        let f: Vec<(usize, usize)> = std::fs::read_to_string(filename)?
+            .split(',')
+            .map(|rng| {
+                let tmp: Vec<usize> = rng
+                    .trim()
+                    .split('-')
+                    .map(|n| n.parse::<usize>().unwrap())
+                    .collect();
+
+                if tmp.len() != 2 {
+                    panic!("Replace me with an error ret later. (TMP.LEN != 2)");
+                }
+
+                (tmp[0], tmp[1])
+            })
+            .collect();
+
+        Ok(Aoc2025Day2 { ranges: f })
+    }
+
+    fn run(&mut self) -> Result<(usize, usize), Box<dyn Error>> {
+        let mut matches_halves_only: Vec<usize> = Vec::new();
+        let mut matches: Vec<usize> = Vec::new();
+
+        for range in &self.ranges {
+            for i in range.0..=range.1 {
+                let i_str = i.to_string();
+                let mut patterns = Vec::new();
+                for x in 1..=i_str.len() / 2 {
+                    let spl = i_str.split_at(x);
+                    patterns.push(spl.0);
+                }
+
+                for pat in patterns {
+                    let (matched, count) = Aoc2025Day2::pat_match(pat, &i_str);
+                    if matched {
+                        if !matches.contains(&i) {
+                            matches.push(i);
+                        }
+                        if count == 2 {
+                            matches_halves_only.push(i);
+                        }
+                    }
+                }
+            }
+        }
+
+        let total = matches_halves_only.iter().sum();
+        let total2: usize = matches.iter().sum();
+        println!("Day 1 Answer is {total}");
+        println!("Day 2 Answer is {total2}");
+
+        Ok((total, total2))
+    }
+
+    fn pat_match(pat: &str, num: &str) -> (bool, usize) {
+        let inst: Vec<&str> = num.split(pat).collect();
+        for i in &inst {
+            if !i.is_empty() {
+                return (false, 0);
+            }
+        }
+
+        (true, inst.len() - 1)
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut day2 = Aoc2025Day2::new("input.txt")?;
+
+    day2.run()?;
+
+    Ok(())
+}
